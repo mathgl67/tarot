cc7a86d5bcb6457887cec9954ef79b7d1da44b4d
mathgl <mathgl67@gmail.com>
2010-09-15 14:29:34 +0200

    fix: licence header and file.

--- tarot/ai/take_config_file.py
+++ tarot/ai/take_config_file.py
@@ -1,8 +1,25 @@
-'''
-Created on 6 juin 2010
+#
+# -*- coding: utf8 -*-
+#
+# Tarot 
+# Copyright (C) 2009-2010  mathgl67@gmail.com
+#
+#  This file is part of Tarot
+#
+#  Tarot is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Tarot is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Tarot.  If not, see <http://www.gnu.org/licenses/>.
+#
 
-@author: mathgl
-'''
 from ConfigParser import RawConfigParser
 from tarot.ai.test.deck import BoutCountDeckTest, TrumpPercentageOfDeckTest
 from tarot.ai.test.deck import CutCountDeckTest, FaceCountByNameDeckTest
